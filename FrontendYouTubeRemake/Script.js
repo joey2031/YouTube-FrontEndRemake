9077650ee70d20138d9057a57189a5caaa3c43c2
joey2031 <36198917+joey2031@users.noreply.github.com>
2019-02-28 15:01:09 -0500

    Add files via upload

---
+++ FrontendYouTubeRemake/Script.js
@@ -0,0 +1,12 @@
+function openSlideMenue(){
+	document.getElementById('menu').style.width = '250px';
+	document.getElementById('content').style.marginLeft = '250px';
+
+}
+
+
+function closeSlideMenue(){
+	document.getElementById('menu').style.width = '0px';
+	document.getElementById('content').style.marginLeft = '0px';
+
+}
