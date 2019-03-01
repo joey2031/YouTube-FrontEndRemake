5b915a0006d10a2d1c01eb5e16e814dfb2214d4c
joey2031 <36198917+joey2031@users.noreply.github.com>
2019-02-28 19:30:29 -0500

    Add files via upload

--- FrontendYouTubeRemake/Script.js
+++ FrontendYouTubeRemake/Script.js
@@ -1,12 +1,48 @@
+
+
 function openSlideMenue(){
+
 	document.getElementById('menu').style.width = '250px';
-	document.getElementById('content').style.marginLeft = '250px';
+	document.getElementById('navContainer').style.marginLeft = '250px';
+
 
 }
 
 
 function closeSlideMenue(){
 	document.getElementById('menu').style.width = '0px';
-	document.getElementById('content').style.marginLeft = '0px';
+	document.getElementById('navContainer').style.marginLeft = '0px';
 
 }
+
+
+
+
+/*
+
+need to see how to do this with one button.
+function openCloseSlideMenue(){
+		let open = false;
+
+
+	if (open == false){
+	document.getElementById('menu').style.width = '250px';
+	document.getElementById('navContainer').style.marginLeft = '250px';
+	open = true;
+	//alert("open");
+
+
+	}
+
+	if (open == true){
+	document.getElementById('menu').style.width = '0px';
+	document.getElementById('navContainer').style.marginLeft = '0px';
+	open = false;
+
+	}
+
+
+
+}
+
+*/
