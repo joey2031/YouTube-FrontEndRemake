246727e76bcb7609c8438745b74b98cdb0e5c10e
joey2031 <36198917+joey2031@users.noreply.github.com>
2019-03-02 11:57:28 -0500

    Add files via upload

--- FrontendYouTubeRemake/Script.js
+++ FrontendYouTubeRemake/Script.js
@@ -1,17 +1,20 @@
-
+// for over lay //www.w3schools.com/howto/howto_js_sidenav.asp
+//Better idea to add a overlay https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_overlay
 
 function openSlideMenue(){
 
 	document.getElementById('menu').style.width = '250px';
-	document.getElementById('navContainer').style.marginLeft = '250px';
+	document.getElementById('navContainer').style.marginLeft = '0px';
+	//document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 
 
 }
 
 
 function closeSlideMenue(){
-	document.getElementById('menu').style.width = '0px';
+	document.getElementById('menu').style.width = '0';
 	document.getElementById('navContainer').style.marginLeft = '0px';
+	//document.body.style.backgroundColor = "white";
 
 }
 
